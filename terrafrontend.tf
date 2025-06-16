dde04b2b85c9b092957db27c777ba2b32ccce48f
Balaphani <phnkumar51@gmail.com>
2025-06-16 17:52:12 +0530

    terra frontedn

--- terrafrontend.tf
+++ terrafrontend.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "terrafrontend" {
   ami           = "ami-09e6f87a47903347c"
   instance_type = "t3.small"
-  vpc_security_group_ids = [sg-025f5fd9e7683454e]
+  vpc_security_group_ids = ["sg-025f5fd9e7683454e"]
 
   tags = {
     Name = "terrafrontend"
